923a42624a064c643c78830bb3b59a00075feecb
Michael Li <michael.li04@sap.com>
2020-05-20 19:28:19 +0800

    fix compile

--- src/main/java/com/sap/tut/gcp/Kayak.java
+++ src/main/java/com/sap/tut/gcp/Kayak.java
@@ -1,15 +1,23 @@
 package com.sap.tut.gcp;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
 
 @Document
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class Kayak {
+	
+	public Kayak() {
+		
+	}
+	
+	public Kayak(String n, String o, Number v, String m) {
+		this.name = n;
+		this.owner = o;
+		this.value = v;
+		this.makeModel = m;
+	}
 
     private String name;
 
